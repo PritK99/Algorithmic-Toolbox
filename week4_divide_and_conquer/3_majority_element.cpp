4eeef54305c4e8dfa41fcdee6b059bf592d03ef4
PritK99 <pritpjk@gmail.com>
2023-05-20 19:34:18 +0530

    Adding implementation for majority element

--- week4_divide_and_conquer/3_majority_element.cpp
+++ week4_divide_and_conquer/3_majority_element.cpp
@@ -1,22 +1,104 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
-using std::vector;
+using namespace std;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+void Merge(vector<int> &A, int left, int right, int mid)
+{
+    int p = left;
+    int q = mid;
+
+    vector<int> merged;
+
+    while (p < mid && q < right)
+    {
+        if (A[p] <= A[q])
+        {
+            merged.push_back(A[p]);
+            p++;
+        }
+        else
+        {
+            merged.push_back(A[q]);
+            q++;
+        }
+    }
+
+    while (p < mid)
+    {
+        merged.push_back(A[p]);
+        p++;
+    }
+    while (q < right)
+    {
+        merged.push_back(A[q]);
+        q++;
+    }
+
+    for (int i = 0; i < merged.size(); i++)
+    {
+        A[left + i] = merged[i];
+    }
 }
 
-int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+void MergeSort(vector<int> &A, int begin, int end)
+{
+    if (end - begin == 1)
+    {
+        return;
+    }
+    int mid = floor((end - begin) / 2);
+    MergeSort(A, begin, begin + mid);
+    MergeSort(A, begin + mid, end);
+    Merge(A, begin, end, begin+mid);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    MergeSort(v, 0, v.size());
+
+    bool flag = 0 ;
+    int tmp = (v.size()/2) ;
+    for (int i = 1 ; i < v.size() ; i++)
+    {
+      // cout << v[i]  << endl ;
+      if (tmp == 0)
+      {
+        cout << 1 ;
+        flag = 1 ;
+        break ;
+      }
+      if (v[i-1] == v[i])
+      {
+        // cout << "Good\n";
+        tmp -- ;
+      }
+      else 
+      {
+        tmp = (v.size()/2) ;
+      }
+    }
+
+    if (tmp == 0 && flag == 0)
+    {
+      cout << 1 ;
+      flag = 1 ;
+    }
+    if (flag == 0)
+    {
+      cout << 0 ;
+    }
+    
+    return 0;
 }
---
+++ week4_divide_and_conquer/README.md
@@ -0,0 +1,8 @@
+## Week 4: Divide & Conquer
+
+## Accepted solutions
+
+* ```1_binary_search.cpp```
+* ```2_binary_search_duplicate.cpp```
+* ```3_majority_element.cpp```
+* ```5_number_of_inversions.cpp```
